25573e12c17d1a4453ed334f99722758922b399a
Lara <lara.maria.meister@hs-furtwangen.de>
2022-05-14 12:28:35 +0200

    fliegender button for later adding players etc

--- ComBEERtition/app/src/main/java/com/example/combeertition/feature/main/ui/MainScreen.kt
+++ ComBEERtition/app/src/main/java/com/example/combeertition/feature/main/ui/MainScreen.kt
@@ -2,23 +2,29 @@ package com.example.combeertition.feature.main.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.combeertition.feature.main.navigation.BottomNavigationItemApp
 import com.example.combeertition.feature.main.navigation.MainBottomNavigation
 import com.example.combeertition.feature.main.navigation.MainNavigationGraph
+import com.example.combeertition.R
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val currentRoute = navController.currentBackStackEntryFlow
+        .collectAsState(initial = navController.currentBackStackEntry)
 
     Scaffold(
         topBar = {
@@ -34,6 +40,26 @@ fun MainScreen() {
                 },
             )
         },
+            floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {
+                            when (currentRoute.value?.destination?.route) {
+                                //Overlay öffnen
+                                "competitons" ->  {  }
+                                "teams" -> { }
+                                "players" -> { }
+                            }
+                        },
+                        backgroundColor = Color(0xFFFFDE59),
+                        content = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_add_24),
+                                contentDescription = null,
+                                modifier = Modifier.size(28.dp)
+                            )
+                        }
+                    )
+            },
         bottomBar = { MainBottomNavigation(navController) }
     ) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
