e2134c8b8ce36a47be43d38cfcc36bd6c89447ba
Lara <lara.maria.meister@hs-furtwangen.de>
2022-05-14 13:46:25 +0200

    style nav

--- ComBEERtition/app/src/main/java/com/example/combeertition/feature/main/navigation/MainBottomNavigation.kt
+++ ComBEERtition/app/src/main/java/com/example/combeertition/feature/main/navigation/MainBottomNavigation.kt
@@ -1,17 +1,21 @@
 package com.example.combeertition.feature.main.navigation
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun MainBottomNavigation(navController: NavController) {
-    BottomNavigation {
+    BottomNavigation(modifier = Modifier.height(64.dp)) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
@@ -23,7 +27,9 @@ fun MainBottomNavigation(navController: NavController) {
             BottomNavigationItem(
                 selected = currentRoute == navItem.routeName,
                 icon = {
-                        Icon(painter = painterResource(navItem.icon), contentDescription = navItem.routeName)
+                        Icon(painter = painterResource(navItem.icon),
+                            modifier = Modifier.size(44.dp),
+                            contentDescription = navItem.routeName)
                 },
                 onClick = {
                     navController.navigate(navItem.routeName) {
