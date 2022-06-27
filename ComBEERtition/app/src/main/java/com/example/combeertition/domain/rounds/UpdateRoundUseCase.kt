e4c5f48a8c0a489ed678c457dfbcae6431b014c3
Lara <lara.maria.meister@hs-furtwangen.de>
2022-06-27 23:05:05 +0200

    set statistic

--- ComBEERtition/app/src/main/java/com/example/combeertition/domain/rounds/UpdateRoundUseCase.kt
+++ ComBEERtition/app/src/main/java/com/example/combeertition/domain/rounds/UpdateRoundUseCase.kt
@@ -2,12 +2,10 @@ package com.example.combeertition.domain.rounds
 
 import androidx.compose.ui.graphics.Color
 import com.example.combeertition.R
+import com.example.combeertition.data.playerRepository
 import com.example.combeertition.data.roundsRepository
 import com.example.combeertition.data.teamRepository
-import com.example.combeertition.domain.model.Round
-import com.example.combeertition.domain.model.RoundId
-import com.example.combeertition.domain.model.Team
-import com.example.combeertition.domain.model.TeamId
+import com.example.combeertition.domain.model.*
 
 class UpdateRoundUseCase {
     suspend operator fun invoke(roundId: RoundId, pointsFirst: Int, pointsSecond: Int): Boolean {
@@ -28,6 +26,72 @@ class UpdateRoundUseCase {
                         pointsSecond
                     )
                 )
+                var team = teamRepository.getTeamById(TeamId(updatedRound.firstTeam))
+
+                if (team != null) {
+                    teamRepository.updateTeam(
+                        Team.create(
+                            team.id,
+                            team.name,
+                            team.icon,
+                            team.color,
+                            team.players,
+                            team.wins,
+                            team.looses + 1,
+                            team.matches + 1
+                        )
+                    )
+
+                    for (player in team.players) {
+                        var player = playerRepository.getPlayerById(PlayerId(player))
+                        if (player != null) {
+                            playerRepository.updatePlayer(
+                                Player.create(
+                                    player.id,
+                                    player.name,
+                                    player.icon,
+                                    player.color,
+                                    player.wins,
+                                    player.looses + 1,
+                                    player.matches + 1
+                                )
+                            )
+                        }
+                    }
+                }
+
+
+                team = teamRepository.getTeamById(TeamId(updatedRound.secondTeam))
+                if (team != null) {
+                    teamRepository.updateTeam(
+                        Team.create(
+                            team.id,
+                            team.name,
+                            team.icon,
+                            team.color,
+                            team.players,
+                            team.wins + 1,
+                            team.looses,
+                            team.matches + 1
+                        )
+                    )
+                    for (player in team.players) {
+                        var player = playerRepository.getPlayerById(PlayerId(player))
+                        if (player != null) {
+                            playerRepository.updatePlayer(
+                                Player.create(
+                                    player.id,
+                                    player.name,
+                                    player.icon,
+                                    player.color,
+                                    player.wins + 1,
+                                    player.looses,
+                                    player.matches + 1
+                                )
+                            )
+                        }
+                    }
+                }
             } else if (pointsSecond == 0) {
                 roundsRepository.updateRound(
                     Round.create(
@@ -42,7 +106,69 @@ class UpdateRoundUseCase {
                         pointsSecond
                     )
                 )
+                var team = teamRepository.getTeamById(TeamId(updatedRound.firstTeam))
 
+                if (team != null) {
+                    teamRepository.updateTeam(
+                        Team.create(
+                            team.id,
+                            team.name,
+                            team.icon,
+                            team.color,
+                            team.players,
+                            team.wins + 1,
+                            team.looses,
+                            team.matches + 1
+                        )
+                    )
+                    for (player in team.players) {
+                        var player = playerRepository.getPlayerById(PlayerId(player))
+                        if (player != null) {
+                            playerRepository.updatePlayer(
+                                Player.create(
+                                    player.id,
+                                    player.name,
+                                    player.icon,
+                                    player.color,
+                                    player.wins + 1,
+                                    player.looses,
+                                    player.matches + 1
+                                )
+                            )
+                        }
+                    }
+                }
+                team = teamRepository.getTeamById(TeamId(updatedRound.secondTeam))
+                if (team != null) {
+                    teamRepository.updateTeam(
+                        Team.create(
+                            team.id,
+                            team.name,
+                            team.icon,
+                            team.color,
+                            team.players,
+                            team.wins,
+                            team.looses + 1,
+                            team.matches + 1
+                        )
+                    )
+                    for (player in team.players) {
+                        var player = playerRepository.getPlayerById(PlayerId(player))
+                        if (player != null) {
+                            playerRepository.updatePlayer(
+                                Player.create(
+                                    player.id,
+                                    player.name,
+                                    player.icon,
+                                    player.color,
+                                    player.wins,
+                                    player.looses + 1,
+                                    player.matches + 1
+                                )
+                            )
+                        }
+                    }
+                }
             } else {
                 roundsRepository.updateRound(
                     Round.create(
